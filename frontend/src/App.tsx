a390f731b02b401675ff2e3263d6a3fc603de3a1
binod maharjan <maharjan411@gmail.com>
2024-11-26 18:41:58 +0545

    Components from shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+
+import Views from './pages/View'
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+     <Views/>
+    </BrowserRouter>
+  )
+}
+
+export default App
