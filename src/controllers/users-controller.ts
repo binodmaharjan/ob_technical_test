aea0774e2ed4662c9fbab9406235e8fd1b7c363a
binod maharjan <maharjan411@gmail.com>
2024-11-26 18:36:31 +0545

    Update users-controller.ts

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,48 +1,51 @@
 // src/controllers/usersController.ts
 
 import { Request, Response } from 'express';
+import { RecommendationModel } from '../models/recommendation';
 // import { PromotionModel } from '../models/Promotion';
 
-export const getClientPromotions = async (req: Request, res: Response) => {
-  /**
+/**
    * TODO: Implement this controller function.
    *
    * Steps:
-   * 1. Extract `clientId` from `req.params`.
-   * 2. Retrieve the promotions for the given `clientId` from the database.
-   * 3. If promotions exist, return them in the response.
+   * 1. Extract `user_id` from `req.params`.
+   * 2. Retrieve the promotions for the given `user_id` from the database.
+   * 3. If recommendations exist, return them in the response.
    * 4. If not, return a 404 error with an appropriate message.
    *
    * Handle exceptions and errors appropriately.
    *
    * Hints:
-   * - Use `PromotionModel` to query the database.
+   * - Use `RecommendationModel` to query the database.
    * - Use try-catch blocks to handle exceptions.
    */
-
-  // Example (from a different context):
-
-  /*
-  const clientId = req.params.clientId;
-
+export const getUserRecommendations = async (req: Request, res: Response) => {
+  const { user_id } = req.params ;
   try {
-    const promotion = await PromotionModel.findOne({ clientId });
-
-    if (!promotion) {
-      return res.status(404).json({
-        error: `No promotions found for client ${clientId}.`,
+    const recommendations =  await RecommendationModel.find({ user_id });
+    if (!recommendations || recommendations.length === 0) {
+       res.status(404).json({
+        error: `No recommendations found for user ${user_id}.`,
       });
+      return
     }
 
-    res.json({
-      clientId,
-      promotions: promotion.promotions,
-    });
+
+    //sanitize the recommendations
+    const recommendationsArr = recommendations.map((recommendation) => {
+      return  recommendation.suggestion
+    })
+
+    const finalRes = {
+      user_id,
+      recommendations: recommendationsArr,
+    }
+
+    return res.status(200).json(finalRes);
   } catch (error) {
-    console.error('Error fetching promotions:', error);
-    res.status(500).json({
-      error: 'Unable to fetch promotions at this time. Please try again later.',
+    console.error('Error fetching recommendations:', error);
+    return res.status(500).json({
+      error: 'Unable to fetch recommendations at this time. Please try again later.',
     });
   }
-  */
-};
+}
